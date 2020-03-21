b5d5e5bf84164e47f247ea49223fb3c026a76ea7
Myroslav Dmytrus <myroslavdm@gmail.com>
2020-03-21 17:35:22 +0200

    Created init react app

---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Home from './scenes/Home';
+
+render(<Home />, document.getElementById('root'));
---
+++ frontend/src/scenes/Home/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        Home scene
+    </div>
+);
+
+export default Home;
