29de7cc3d918cac3ff41b518deb5c8f3f9751d64
Myroslav Dmytrus <myroslavdm@gmail.com>
2020-03-21 16:58:41 +0200

    Created Spring Java backend project

---
+++ backend/src/main/java/com/threadjava/ThreadJavaApplication.java
@@ -0,0 +1,13 @@
+package com.threadjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadJavaApplication.class, args);
+	}
+
+}
