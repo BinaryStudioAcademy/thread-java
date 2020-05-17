77646f5a675ce502bcee31c9c916df3510d79985
Myroslav Dmytrus <myroslavdm@gmail.com>
2020-05-17 13:47:54 +0300

    Updated react app

--- frontend/src/components/Notifications/index.jsx
+++ frontend/src/components/Notifications/index.jsx
@@ -1,71 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import { NotificationContainer, NotificationManager } from 'react-notifications';
-
 import 'react-notifications/lib/notifications.css';
 
-class Notifications extends React.Component {
-    socket = undefined;
-    stompClient = undefined;
+const Notifications = ({ user, applyPost }) => {
 
-    componentDidMount() {
-        this.initSocket();
+  const initSocket = () => new Promise((resolve, reject) => {
+    const socket = new SockJS("/ws");
+    const stompClient = Stomp.over(socket);
+    stompClient.debug = () => { };
+    stompClient.connect({}, () => {
+      console.log('connected');
+      resolve(stompClient);
+    });
+  });
+
+
+  useEffect(async () => {
+    if (!user) {
+      return undefined;
     }
 
-    componentDidUpdate(prevProps) {
-        this.initSocket();
-        this.checkLoggedInUser(prevProps);
-    }
+    let stompClient = await initSocket();
 
-    checkLoggedInUser = (prevProps) => {
-        if (this.socket && !this.props.user && this.props.user !== prevProps.user) {
-            // this.socket.emit('leaveRoom', prevProps.user.id);
-            this.stompClient.disconnect()
-        }
-    }
+    const { id } = user;
 
-    addSocketHandlers = (userId) => {
-        // this.socket.emit('createRoom', userId);
-        this.stompClient.subscribe('/topic/like', () => {
-            NotificationManager.info('Your post was liked!');
-        });
-        this.stompClient.subscribe('/topic/new_post', (message) => {
-            let post = JSON.parse(message.body);
-            console.log('message from server2: ' + post);
-            if (post.userId !== userId) {
-                this.props.applyPost(post.id);
-            }
-        });
-    }
+    stompClient.subscribe('/topic/like', () => {
+      NotificationManager.info('Your post was liked!');
+    });
 
-    initSocket() {
-        const { user } = this.props;
-        if (!this.socket && user && user.id) {
-            this.socket = new SockJS("/ws");
-            this.stompClient = Stomp.over(this.socket);
-            this.stompClient.debug = () => { };
-            this.stompClient.connect({}, () => {
-                this.addSocketHandlers(user.id);
-            });
-        }
-    }
+    stompClient.subscribe('/topic/new_post', (message) => {
+      let post = JSON.parse(message.body);
+      console.log('message from server2: ' + post);
+      if (post.userId !== id) {
+        applyPost(post.id);
+      }
+    });
 
-    render() {
-        return <NotificationContainer>
+    return () => {
+      stompClient.disconnect();
+    };
+  });
 
-        </NotificationContainer>;
-    }
-}
+  return <NotificationContainer />;
+};
 
 Notifications.defaultProps = {
-    user: {}
+  user: undefined
 };
 
 Notifications.propTypes = {
-    user: PropTypes.objectOf(PropTypes.any),
-    applyPost: PropTypes.func.isRequired,
+  user: PropTypes.objectOf(PropTypes.any),
+  applyPost: PropTypes.func.isRequired
 };
 
 export default Notifications;
