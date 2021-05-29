f7dd3df43c9dbf8142028b72abf4b5cce7e5a4ec
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2021-05-29 19:46:05 +0300

    fix post and user prop-types

--- frontend/src/common/prop-types/user.js
+++ frontend/src/common/prop-types/user.js
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
+import { imageType } from './image';
 
 const userType = PropTypes.exact({
   id: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
-  image: PropTypes.string,
+  image: imageType,
   imageId: PropTypes.string,
   username: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired,
-  updatedAt: PropTypes.string.isRequired
+  createdAt: PropTypes.string,
+  updatedAt: PropTypes.string
 });
 
 export { userType };
